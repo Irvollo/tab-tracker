2db8b0c79d3f58c5e54aebbf7a7e32e338350029
irvollo <irvollo@hotmail.com>
2018-01-31 23:15:48 -0600

    Create song and index for songs

---
+++ client/src/components/CreateSong.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <v-layout>
+      <v-flex xs4>
+      <panel title="Song Metadata">
+          <v-text-field
+            label="Title"
+            required
+            :rules="[required]"
+            v-model="song.title">
+          </v-text-field>
+
+          <v-text-field
+            label="Artist"
+            required
+            :rules="[required]"
+            v-model="song.artist">
+          </v-text-field>
+
+          <v-text-field
+            label="Genre"
+            required
+            :rules="[required]"
+            v-model="song.genre">
+          </v-text-field>
+
+          <v-text-field
+            label="Album"
+            required
+            :rules="[required]"
+            v-model="song.album">
+          </v-text-field>
+
+          <v-text-field
+            label="Image URL"
+            required
+            :rules="[required]"
+            v-model="song.albumImageUrl">
+          </v-text-field>
+
+          <v-text-field
+            label="Youtube ID"
+            required
+            :rules="[required]"
+            v-model="song.youtubeID">
+          </v-text-field>
+        </panel>
+      </v-flex>
+      <v-flex xs12>
+        <panel title="Song Structure" class="ml-4">
+          <v-text-field
+              label="Lyrics"
+              multi-line
+              required
+              :rules="[required]"
+              v-model="song.lyrics">
+            </v-text-field>
+
+            <v-text-field
+              label="Tab"
+              multi-line
+              required
+              :rules="[required]"
+              v-model="song.tab">
+            </v-text-field>
+          </panel>
+
+          <v-alert
+            class="ml-4"
+            :value="error"
+            transition="scale-transition"
+            error>
+              {{error}}
+          </v-alert>
+
+           <v-btn
+            @click="create">
+            Create
+          </v-btn>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+import SongService from '@/services/SongService'
+export default {
+  data () {
+    return {
+      song: {
+        title: null,
+        artist: null,
+        genre: null,
+        album: null,
+        albumImageUrl: null,
+        youtubeID: null,
+        lyrics: null,
+        tab: null
+      },
+      error: null,
+      required: (value) => !!value || 'Required'
+    }
+  },
+  components: {
+    Panel
+  },
+  methods: {
+    async create () {
+      this.error = null
+      const areAllFieldsFilledIn = Object.keys(this.song).every(key => !!this.song[key])
+      if (!areAllFieldsFilledIn) {
+        this.error = 'Please fill in all the required fields'
+        return
+      }
+      try {
+        await SongService.post(this.song)
+        this.$router.push({
+          name: 'songs'
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,10 +2,47 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <panel title="Songs">
-        <div v-for="song in songs" :key="song.id">
-          {{song.title}} -
-          {{song.artist}} -
-          {{song.album}}
+        <v-btn
+          slot="action"
+          @click="navigateTo({name:'songs-create'})"
+          class="orange"
+          dark
+          medium
+          absolute
+          right
+          middle
+          fab>
+            <v-icon>add</v-icon>
+        </v-btn>
+        <div class="song" v-for="song in songs" :key="song.id">
+
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-genre">
+                {{song.album}}
+              </div>
+              <v-btn
+                @click="navigateTo({
+                  name:'song',
+                  params: {
+                    songId: song.id
+                  }
+                })"
+                class="orange"
+                dark>
+                  View
+              </v-btn>
+            </v-flex>
+            <v-flex xs6>
+              <img class="album-image" :src="song.albumImageUrl" />
+            </v-flex>
+          </v-layout>
         </div>
       </panel>
     </v-flex>
@@ -21,6 +58,11 @@ export default {
       songs: null
     }
   },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  },
   components: {
     Panel
   },
@@ -32,5 +74,27 @@ export default {
 </script>
 
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
 
+.song-title {
+  font-size: 30px;
+}
+
+.song-artist {
+  font-size:24px;
+}
+
+.song-genre {
+  font-size: 18px;
+}
+
+
+.album-image {
+  width:  70%;
+  margin: 0 auto;
+}
 </style>
